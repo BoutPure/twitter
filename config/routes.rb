3690094b387811c838c2a9d29f68fd4915c8bde7
a.vakulenko <a.vakulenko@21-school.ru>
2022-06-14 14:00:01 +0300

    feat: setup devise gem, add simple pages to test it

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index", as: 'user_root'
+  get "/secured_page", to: "articles#secured_page"
+
+  # devise_scope :user do
+  #   delete 'logout', to: 'devise/sessions#destroy'
+  # end
 end
